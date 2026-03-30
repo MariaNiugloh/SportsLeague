a89d708e91863ed8d93e367d5491dee96b8f0aa8
Camila Holguín <camilaholguinmrpo@gmail.com>
2026-03-29 22:20:00 -0500

    Add TournamentSponsor entity for many to many relation

---
+++ SportsLeague.Domain/Entities/TournamentSponsor.cs
@@ -0,0 +1,20 @@
+﻿namespace SportsLeague.Domain.Entities
+{
+    public class TournamentSponsor
+    {
+        public int Id { get; set; }
+
+        public int TournamentId { get; set; }
+
+        public int SponsorId { get; set; }
+
+        public decimal ContractAmount { get; set; }
+
+        public DateTime JoinedAt { get; set; }
+
+        // Navigation Properties
+        public Tournament Tournament { get; set; } = null!;
+
+        public Sponsor Sponsor { get; set; } = null!;
+    }
+}
