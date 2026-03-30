3a75241e9ac1f1fe5e6412abbcb1ee039d327643
Camila Holguín <camilaholguinmrpo@gmail.com>
2026-03-30 16:10:00 -0500

    Add Sponsor DTOs

---
+++ SportsLeague.API/DTOs/Request/SponsorRequestDTO.cs
@@ -0,0 +1,12 @@
+﻿using SportsLeague.Domain.Enums;
+
+namespace SportsLeague.API.DTOs;
+
+public class SponsorRequestDTO
+{
+    public string Name { get; set; } = null!;
+    public string ContactEmail { get; set; } = null!;
+    public string? Phone { get; set; }
+    public string? WebsiteUrl { get; set; }
+    public SponsorCategory Category { get; set; }
+}
